e3cc9a5a9e70f17f0daadd71566d2c926e8924e5
narcisolobo <narcisolobo@gmail.com>
2023-06-11 06:15:35 -0700

    installed vercel and prisma

--- src/routes/api/jokes/+server.ts
+++ src/routes/api/jokes/+server.ts
@@ -1,10 +1,20 @@
 import { json } from '@sveltejs/kit';
+import { PrismaClient } from '@prisma/client';
 
-interface Joke {
-	setup: string;
-	punchline: string;
+const prisma = new PrismaClient();
+
+export async function GET() {
+	const allJokes = await prisma.joke.findMany();
+	return json(allJokes, { status: 200 });
 }
 
-export function GET() {
-	return json({ message: 'hello world' });
+export async function POST({ request }) {
+	const { setup, punchline } = await request.json();
+	const newJoke = await prisma.joke.create({
+		data: {
+			setup: setup,
+			punchline: punchline
+		}
+	});
+	return json(newJoke, { status: 201 });
 }
